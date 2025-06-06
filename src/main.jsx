aedc51ab6c291d69c24852682aed7f2f62f619ba
QuyenDz <qh10072504@gmail.com>
2025-06-06 21:55:06 +0700

    feat: Implement authentication APIs and enhance login/signup forms with validation

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,13 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
 
+//react-toastify
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
+    <ToastContainer position="bottom-left" />
   </BrowserRouter>,
 )
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import { useNavigate } from 'react-router'
 import { Link } from 'react-router'
+import { useForm } from 'react-hook-form'
+import { toast } from 'react-toastify'
+import { registerAPI } from '../apis/auth'
 
 const Signup = () => {
+  const navigate = useNavigate()
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
+
+  const signUp = (data) => {
+    const { name, email, phone, password } = data
+
+    console.log(password, data.confirmPassword)
+
+    toast
+      .promise(registerAPI({ name, email, password, phone }), {
+        pending: 'Registration is in progress...',
+      })
+      .then((res) => {
+        navigate(`/login?registeredEmail=${res.email}`)
+      })
+  }
+
   return (
     <section className="bg-white min-h-screen">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen">
@@ -18,12 +44,44 @@ const Signup = () => {
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900">
               Tạo tài khoản
             </h1>
-            <form className="space-y-4">
+            <form className="space-y-4" onSubmit={handleSubmit(signUp)}>
+              <div>
+                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900">
+                  Username
+                </label>
+                <input
+                  {...register('name', {
+                    required: true,
+                    minLength: {
+                      value: 3,
+                      message: 'Username phải có ít nhất 3 ký tự',
+                    },
+                  })}
+                  type="name"
+                  name="name"
+                  id="name"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                  placeholder="quyenbv..."
+                  required
+                />
+                {errors.name && (
+                  <p className="text-red-500 text-sm mt-1">
+                    {errors.name.message || 'Ten là bắt buộc'}
+                  </p>
+                )}
+              </div>
               <div>
                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">
                   Email
                 </label>
                 <input
+                  {...register('email', {
+                    required: true,
+                    pattern: {
+                      value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                      message: 'Email không hợp lệ',
+                    },
+                  })}
                   type="email"
                   name="email"
                   id="email"
@@ -31,12 +89,49 @@ const Signup = () => {
                   placeholder="name@company.com"
                   required
                 />
+                {errors.email && (
+                  <p className="text-red-500 text-sm mt-1">
+                    {errors.email.message || 'Email là bắt buộc'}
+                  </p>
+                )}
+              </div>
+              <div>
+                <label htmlFor="phone" className="block mb-2 text-sm font-medium text-gray-900">
+                  Phone
+                </label>
+                <input
+                  {...register('phone', {
+                    required: true,
+                    pattern: {
+                      value: /^\d{10,11}$/,
+                      message: 'So dien thoai không hợp lệ',
+                    },
+                  })}
+                  type="phone"
+                  name="phone"
+                  id="phone"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                  placeholder="name@company.com"
+                  required
+                />
+                {errors.phone && (
+                  <p className="text-red-500 text-sm mt-1">
+                    {errors.phone.message || 'So dien thoai là bắt buộc'}
+                  </p>
+                )}
               </div>
               <div>
                 <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">
                   Mật khẩu
                 </label>
                 <input
+                  {...register('password', {
+                    required: true,
+                    minLength: {
+                      value: 6,
+                      message: 'Mật khẩu phải có ít nhất 6 ký tự',
+                    },
+                  })}
                   type="password"
                   name="password"
                   id="password"
@@ -44,6 +139,11 @@ const Signup = () => {
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                   required
                 />
+                {errors.password && (
+                  <p className="text-red-500 text-sm mt-1">
+                    {errors.password.message || 'Mật khẩu là bắt buộc'}
+                  </p>
+                )}
               </div>
               <div>
                 <label
@@ -53,24 +153,22 @@ const Signup = () => {
                   Nhập lại mật khẩu
                 </label>
                 <input
+                  {...register('confirmPassword', {
+                    required: true,
+                    validate: (value) => value === watch('password') || 'Mật khẩu không khớp',
+                  })}
                   type="password"
-                  name="confirm-password"
+                  name="confirmPassword"
                   id="confirm-password"
                   placeholder="••••••••"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                   required
                 />
+                {errors.confirmPassword && (
+                  <p className="text-red-500 text-sm mt-1">{errors.confirmPassword.message}</p>
+                )}
               </div>
-              <div className="flex items-start">
-                <div className="flex items-center h-5">
-                  <input
-                    id="terms"
-                    type="checkbox"
-                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-blue-500"
-                    required
-                  />
-                </div>
-              </div>
+
               <button
                 type="submit"
                 className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
